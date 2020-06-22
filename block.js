da009919f973cafa8ab71ada1472deb7646b0a2a
shiviesharma <52064270+shiviesharma@users.noreply.github.com>
2020-06-22 19:55:33 +0530

    Add files via upload

---
+++ block.js
@@ -0,0 +1,12 @@
+class block{
+    constructor(){
+        restitution = 0.8,
+        friction = 1,
+        density = 0.5
+
+
+        this.width = width;
+        this.height = height;
+    }
+
+}
